e2e0e375b7d51fce5151d24791cd9538a92a5593
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-05-19 14:11:38 -0700

    added student course association and ability for students to enroll in courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: { minimum: 5, maximum: 10 }
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
   validates :description, presence: true, length: {minimum: 15, maximum: 300 }
+
+  has_many :student_courses
+  has_many :students, through: :student_courses
+
 end
