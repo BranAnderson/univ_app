21785aec1e6157d1b1e9f7f13dc7c0565797db67
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-05-19 13:14:21 -0700

    added login and logout with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'courses#index'
-  get 'about', to: 'pages#about'
   get 'courses/new', to:'courses#new'
+
+  get 'about', to: 'pages#about'
+  
   resources :students, except: [:destroy]
+
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
+
 end
